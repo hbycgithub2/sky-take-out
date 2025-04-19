201f5f268824f159249baf733476e5291556807e
hbycgithub2 <1311768852@qq.com>
2025-04-19 22:38:16 +0800

    feat(admin): 新增菜品管理功能

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,80 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.system.ApplicationHome;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Slf4j
+@Api(tags = "通用接口")
+public class CommonController {
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    /**
+     * 文件上传
+     *
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file) {
+        log.info(file.getName());
+
+/*        //原始文件名
+        String originalFilename = file.getOriginalFilename();
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        //将文件上传的阿里云
+        String fileName = UUID.randomUUID().toString() + extension;
+        try {
+            String filePath = aliOssUtil.upload(file.getBytes(), fileName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败:{}", e.getMessage());
+        }
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }*/
+
+
+//file校
+        if (file.isEmpty()) {
+            return null;
+            // "图片上传失败";
+        }
+//file重命名(a:1.png b:1.png)
+        String originalFilename = file.getOriginalFilename();//原米的图片名
+        String ext = "." + originalFilename.split("\\.")[1];// 1.png
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        String fileName = uuid + ext;
+//上传图片
+        ApplicationHome applicationHome = new ApplicationHome(this.getClass());
+        String pre = applicationHome.getDir().getParentFile().getParentFile().getAbsolutePath() +
+                //"\\src\\main\\resources\\images\\";//
+                "\\src\\main\\images\\";
+        String path = pre + fileName;
+        try {
+            file.transferTo(new File(path));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.success(path);
+        }
+        return Result.success(path);
+    }
+}
