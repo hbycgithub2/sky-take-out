512de931aac4dd43b238c27d8f3ba294b359ff32
hbycgithub2 <1311768852@qq.com>
2025-04-24 12:25:12 +0800

    feat: 完成用户下单功能

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -9,15 +9,19 @@ public interface UserMapper {
 
     /**
      * 插入
+     *
      * @param user
      */
     void insert(User user);
 
     /**
      * 根据openid查询用户
+     *
      * @return
      */
     @Select("select * from user where openid = #{openid}")
     User getByOpenid(String openid);
 
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
